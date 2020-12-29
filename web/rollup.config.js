b9214f26fda69b241917ca5ff695ded1476aa2e5
jdclarke5 <jdclarke5@gmail.com>
2020-12-30 10:05:34 +1100

    update build to fix external script import

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -2,22 +2,24 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import copy from 'rollup-plugin-copy';
-const htmlPlugin = require('rollup-plugin-html-input');
+import html from '@open-wc/rollup-plugin-html';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
   input: 'index.html',
   output: {
-    file: 'build/bundle.js',
-    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-    sourcemap: false
+    dir: 'build',
+    name: 'bundle',
+    minify: production && true, // minify, but only in production
+    format: 'iife',
+    sourcemap: false,
   },
   plugins: [
     copy({targets: [{src: 'static', dest: 'build'}, {src: 'favicon.ico', dest: 'build'}]}),
-    htmlPlugin(),
+    html(),
     resolve(), // tells Rollup how to find date-fns in node_modules
     commonjs(), // converts date-fns to ES modules
-    production && terser() // minify, but only in production
+    production && terser(), // minify, but only in production
   ]
 };
